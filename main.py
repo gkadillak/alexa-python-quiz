0f52bd489923fe94ac45e2d959b844550424e8f3
Garrett Kadillak <gkadillak@gmail.com>
2018-03-20 00:08:27 -0700

    We have a successful quiz functioning!

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ logger = logging.getLogger(__name__)
 
 QUIZ = None
 
+
 @ask.launch
 def start_skill():
     welcome_message = 'Welcome to Python quiz. Would you like to test your Python knowledge?'
     return question(welcome_message)
+
 
 @ask.on_session_started
 def new_session():
@@ -25,12 +27,16 @@ def new_session():
 
 @ask.intent('AMAZON.YesIntent')
 def start_quiz():
+    return _ask_first_question()
+
+
+def _ask_first_question():
     global QUIZ
-    QUIZ = game.QuizGame(num_questions=1)
+    QUIZ = game.QuizGame(num_questions=2)
     return question(QUIZ.current_question().ask())
 
 
-@ask.intent('QuizAnswerIntent')
+@ask.intent('QuizAnswerIntent', mapping={'guess': 'Answer'})
 def answer(guess):
     return _answer_question(guess)
 
@@ -42,19 +48,15 @@ def _answer_question(guess):
 
     is_answer_correct = QUIZ.answer(guess)
     response = '{response_message} {next_question}'
-    if is_answer_correct:
-        correct_response = 'Correct! Next question:'
-        response = response.format(response=correct_response,
-                                   next_question=QUIZ.current_question().ask())
-        return question(response)
 
     # answering points to a new question - we may be at the end
     if not QUIZ.current_question():
         # statements end the loop between the user and the device
         return statement('The quiz has ended! You got {number_correct} out of {total}'.format(number_correct=QUIZ.number_correct, total=QUIZ.total_questions))
 
-    incorrect_response = 'Incorrect...next question:'
-    response = response.format(response_message=incorrect_response,
+    incorrect_response = 'Incorrect...'
+    correct_response = 'Correct!'
+    response = response.format(response_message=correct_response if is_answer_correct else incorrect_response,
                                next_question=QUIZ.current_question().ask())
     return question(response)
 
