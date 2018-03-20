e9566e2684cd24913e0bbf0547378c1ae62564ee
Garrett Kadillak <gkadillak@gmail.com>
2018-03-20 00:08:27 -0700

    We have a successful quiz functioning!

--- main_test.py
+++ main_test.py
@@ -2,22 +2,18 @@ import json
 import unittest
 
 import main
-import test_data
-
 
 from unittest import mock
 
 from game import game
 
 
-
-
 class PythonQuizTests(unittest.TestCase):
         
 
-    @mock.patch("main.QUIZ", new=game.QuizGame(num_questions=0))
+    @mock.patch("main.QUIZ", new=game.QuizGame(num_questions=1))
     def test_answering_quiz_with_no_questions(self):
         # assert that if there are no more questions, we don't try to ask another
-        main._answer_questions('abc')
-        self.assertTrue(main.QUIZ_GAME.is_complete())
+        main._ask_first_question()
+        self.assertIsNotNone(main._answer_question('abc123'))
         
