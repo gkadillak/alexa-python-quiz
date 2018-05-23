ab9fb5a8f9a31e2741e11dfb2268c31373993a13
Garrett Kadillak <gkadillak@gmail.com>
2018-05-22 22:12:27 -0700

    use correct path for app

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export FLASK_DEBUG=1
-export FLASK_APP='python_quiz/app.py'
+export FLASK_APP='app.py'
 export DATABASE_URL='postgresql://localhost:5432/python_quiz'
 export TESTING_DATABASE_URL='postgresql://localhost:5432/test_python_quiz'
 export APP_SETTINGS='python_quiz.tools.config.DevelopmentConfig'
