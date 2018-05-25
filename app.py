a7252adc812fb50d115b61483daa77c43cf62616
Garrett Kadillak <gkadillak@gmail.com>
2018-05-24 23:19:32 -0700

    Remove unnecessary imports

--- app.py
+++ app.py
@@ -6,14 +6,11 @@ from flask_ask import Ask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from python_quiz.tools import config
-
 flask_app = Flask(__name__)
 flask_app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app=flask_app)
 
 ask = Ask(app=flask_app, route='/python_quiz')
-configs = config
 
 # models need to be imported to track migration changes
 try:
