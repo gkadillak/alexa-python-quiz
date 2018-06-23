276c3ffead70d424f71e1a47260df672fd640884
Garrett Kadillak <gkadillak@gmail.com>
2018-06-23 14:20:57 -0700

    Separate prod and dev envs to fix env variables on prod

--- envs/dev.sh
+++ envs/dev.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-workon quiz_game
+. ../venv/bin/activate
 
 export TESTING_DATABASE_URL='postgresql://localhost:5432/test_python_quiz'
 export APP_SETTINGS='python_quiz.configs.flask_configs.DevelopmentConfig'
 export FLASK_APP='python_quiz/app.py'
+
+
+export DATABASE_URL="postgresql://localhost:5432/python_quiz"
