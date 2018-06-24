8d76e5a0ba135e8e8666041c9c1e4aa5542bdca0
Garrett Kadillak <gkadillak@gmail.com>
2018-06-23 23:51:57 -0700

    Do not mutate the question list when asking, only when answering

--- envs/dev.sh
+++ envs/dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-. ../venv/bin/activate
+. ./venv/bin/activate
 
 export TESTING_DATABASE_URL='postgresql://localhost:5432/test_python_quiz'
 export APP_SETTINGS='python_quiz.configs.flask_configs.DevelopmentConfig'
