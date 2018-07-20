0fac6bb3b80e2872e8ec7458ba9a88afd5f34961
Garrett Kadillak <gkadillak@gmail.com>
2018-07-19 22:14:30 -0700

    No repeat questions (#10)
    * Unique questions per game
    * add attribute to user model to track all questions asked
    * add a unit test

--- python_quiz/game/models/game.py
+++ python_quiz/game/models/game.py
@@ -1,28 +1,8 @@
 from sqlalchemy.dialects import postgresql
-from sqlalchemy.ext.mutable import Mutable
 
 from python_quiz import app
 from python_quiz.game.models import base
-
-
-class MutableList(Mutable, list):
-  def append(self, value):
-    list.append(self, value)
-    self.changed()
-
-  def pop(self, index=0):
-    value = list.pop(self, index)
-    self.changed()
-    return value
-
-  @classmethod
-  def coerce(cls, key, value):
-    if not isinstance(value, MutableList):
-      if isinstance(value, list):
-        return MutableList(value)
-      return Mutable.coerce(key, value)
-    else:
-      return value
+from python_quiz.game.models.column_types import MutableList
 
 
 class Game(base.BaseModel):
--- python_quiz/game/models/user.py
+++ python_quiz/game/models/user.py
@@ -1,5 +1,8 @@
+from sqlalchemy.dialects import postgresql
+
 from python_quiz import app
 from python_quiz.game.models import base
+from python_quiz.game.models.column_types import MutableList
 
 
 class User(base.BaseModel):
@@ -7,5 +10,8 @@ class User(base.BaseModel):
   __tablename__ = 'users'
 
   id = app.db.Column(app.db.String, primary_key=True)
+
+  asked_questions = app.db.Column(MutableList.as_mutable(postgresql.ARRAY(app.db.Integer)), default=[])
+  """All of the questions a user has been asked"""
 
   games = app.db.relationship('Game')
