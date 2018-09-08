a63c6dd6a6aa60f8a6687f7a438a356667f1d993
Garrett Kadillak <gkadillak@gmail.com>
2018-09-08 16:27:28 -0700

    Fix the pop to be last element, add last question answer in response

--- python_quiz/main.py
+++ python_quiz/main.py
@@ -20,7 +20,7 @@ def start_skill():
 def start_quiz():
   session_id = session.get('sessionId')
   user_id = session['user']['userId']
-  game.create_game(num_questions=2, session_id=session_id, user_id=user_id)
+  game.create_game(num_questions=3, session_id=session_id, user_id=user_id)
   rendered_question, question_id = game.ask_current_question(template_name='ask_question', session_id=session_id, user_id=user_id)
   title, content = game.display_card(question_id)
   return question(rendered_question).simple_card(title=title, content=content)
